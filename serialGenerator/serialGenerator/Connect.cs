d012cd3edd60660236b0cfdf605254ffb85b7f63
Dávid <pasztord@kkszki.hu>
2022-09-19 09:30:40 +0200

    Adatbazishoz valo cstalakozas elkeszult

---
+++ serialGenerator/serialGenerator/Connect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace serialGenerator
+{
+    internal class Connect
+    {
+        public MySqlConnection connection;
+
+        string db;
+        string srv;
+        string usr;
+        string pass;
+
+        string connectionstring;
+
+        public Connect()
+        {
+            srv = "localhost";
+            db = "serial";
+            usr = "root";
+            pass = "";
+            connectionstring = "SERVER=" + srv + ";" + "DATABASE=" + db+";" + "UID=" + usr + ";" + "PASSWORD=" + pass + ";" + "SslMode=None;";
+
+            connection=new MySqlConnection(connectionstring);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Sikeres kapcsolódás");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+
+            
+
+    }
+}
---
+++ serialGenerator/serialGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serialGenerator
+{
+    internal class Program
+    {
+        static void kapcsolodas()
+        {
+            Connect c = new Connect();
+        }
+        static void Main(string[] args)
+        {
+            kapcsolodas();
+        }
+    }
+}
